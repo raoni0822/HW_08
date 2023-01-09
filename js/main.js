cc6a0d659549ad349a5fcdd1f8c42e96bbb4d657
Raon Kim <raoni0822@naver.com>
2023-01-09 17:46:40 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.totop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 800)
+    })
+
+    // $('.main_slide').slick({
+    //     arrows: false,
+    //     autoplay: true,
+    //     autoplaySpeed: 1000,
+    // })
+})
